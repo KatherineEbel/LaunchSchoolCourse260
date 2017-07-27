5840c41533fd5e4acfb7637425f9df27782674ba
KatherineEbel <kathyebel@me.com>
2017-07-27 07:37:47 -0400

    load json data and add to view

--- lesson5_buildAMusicStore/musicStore/app.js
+++ lesson5_buildAMusicStore/musicStore/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const path = require('path');
 
 const index = require('./routes/index');
-const users = require('./routes/users');
 
 const app = express();
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- lesson5_buildAMusicStore/musicStore/routes/index.js
+++ lesson5_buildAMusicStore/musicStore/routes/index.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const filePath = path.resolve(path.dirname(__dirname), 'data/albums.json');
+const router = express.Router();
+
+const getAlbums = () => JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { albums: getAlbums() });
 });
 
 module.exports = router;
